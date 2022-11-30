bdb07cd05cdfc0d947c336c16b7b27f30cf23059
Daniel <danieltlyeung@gmail.com>
2022-11-30 14:59:20 -0500

    fixed syntax errors

--- assets/seed.sql
+++ assets/seed.sql
@@ -1,6 +1,6 @@
 SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
 SELECT role.id, role.title FROM role ORDER BY role.id;
-SELECT * FROM employee;
+SELECT * FROM employees;
 
 SELECT department.id, department.name FROM department ORDER BY department.id;
 
